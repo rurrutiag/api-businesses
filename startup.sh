e5347a823c0c95cbcd2ee338c4efe65c9f6d69d5
CorpMail <rodrigo.urrutia@rutiandco.com>
2025-01-03 00:00:38 -0300

    Asegurar instalación de flask en la webapp

--- startup.sh
+++ startup.sh
@@ -1,3 +1,12 @@
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 not found, installing..."
+    curl https://bootstrap.pypa.io/pip/3.7/get-pip.py -o get-pip.py
+    python3 get-pip.py
+fi
 #!/bin/bash
 source .venv/bin/activate
-gunicorn -w 3 -b 0.0.0.0:8000 app:app
+pip install --upgrade pip
+pip install -r requirements.txt
+# gunicorn -w 3 -b 0.0.0.0:8000 app:app
+.venv/bin/gunicorn -w 3 -b 0.0.0.0:8000 app:app
