617609884f3aa5240106dacd90a03f7527462891
CorpMail <rodrigo.urrutia@rutiandco.com>
2025-01-03 16:05:01 -0300

    Puerto de escucha configurado

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def register_new_business_route():
         return jsonify({"message": "Error al registrar el negocio en el microservicio.", "error": str(e)}), 500
 
 if __name__ == '__main__':
-   app.run()
+   port = int(os.environ.get("PORT", 8000))
+   app.run(host="0.0.0.0", port=port)
