bac1f9e293b9af1c9bf93912593aa2678cb2a683
CorpMail <rodrigo.urrutia@rutiandco.com>
2025-01-04 19:00:51 -0300

    Agregando parentesis en las importaciones de flask

--- endpoints/register_new_business.py
+++ endpoints/register_new_business.py
@@ -3,7 +3,7 @@ import os
 import json
 import requests
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import (Flask, request, jsonify)
 from tools.validate_unique_id import generate_unique_branch_ids, generate_unique_company_id, get_all_companies_et_branches_id
 
 # Cargar variables de entorno
